bd5db8431d4a8f6b6f4034524189f9479ed75dd6
Samuel Mueller <mueller.samu@gmail.com>
2016-10-18 22:08:17 +0200

    make translations dependent on base shape coordinates

--- Pythagoras.elm
+++ Pythagoras.elm
@@ -3,35 +3,49 @@ import Transform exposing (Transform)
 import Collage exposing (Form, groupTransform, polygon, filled)
 import Color exposing (rgb)
 import Math exposing (Point)
+import Array exposing (Array, fromList, get)
+-- import Maybe exposing (Maybe)
 
 type alias Model =
   { points : List Point
   , point : Point
-  , edges : List Int
+  , e0 : Int, e1 : Int
+  , e3 : Int, e2 : Int
   }
 
 rectangle : Float -> List (Float, Float)
 rectangle factor =
-  [ (-1, 1), ( 1, 1) , ( 1,-1), (-1,-1) ]
+  [ (-1, 1), ( 0.9, 0.85) , ( 1.1,-1.5), (-1,-1) ]
   |> List.map (\(x, y) -> (x * factor, y * factor))
 
 init : Model
 init =
-  let factor = 20
+  let
+    factor = 20
+    rect = rectangle factor
   in
-    { points = rectangle factor
+    { points = rect
     , point = (factor * -0.3, factor * 2)
-    , edges = [0,1,2,3]
+    , e0 = 0, e1 = 1
+    , e3 = 3, e2 = 2
     }
+
+getPoint : List Point -> Int -> Point
+getPoint points n =
+  case fromList points |> get n of
+    Just a -> a
+    Nothing -> (0,0)
 
 getTransformationMatrix : Model -> Transform
 getTransformationMatrix model =
   let
-    m1 = Transform.translation -20 20
+    (p1x, p1y) = getPoint model.points 1
+    (p2x, p2y) = getPoint model.points 2
+    m1 = Transform.translation -p2x -p2y
     m2 = Transform.rotation (degrees -22)
     m3 = Transform.scale 0.8
-    m4 = Transform.translation 20 -20
-    m5 = Transform.translation 0 40
+    m4 = Transform.translation p2x p2y
+    m5 = Transform.translation (p1x-p2x) (p1y-p2y)
   in
     List.foldl Transform.multiply Transform.identity [m1, m2, m3, m4, m5]
 
